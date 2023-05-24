798134a6ce3c74e721decde03d4af67c945dd622
DadoGC715 <jose.gutierrez.conejo@gmail.com>
2023-05-24 10:17:38 -0600

    DEADLOCK

---
+++ Errors/Deadlock/apply_producer_sponsor_points_SP Original.sql
@@ -0,0 +1,72 @@
+-----------------------------------------------------------
+-- Autor: Eduardo Gutierrez
+-- Fecha: 05/01/2023
+-- Descripcion: esta description en comentarios queda almacenada
+-- Otros detalles de los parametros
+-----------------------------------------------------------
+CREATE PROCEDURE apply_producer_sponsor_points_SP
+	@zip_code INT,
+	@sponsor_points INT
+WITH ENCRYPTION
+AS 
+BEGIN
+	
+	SET NOCOUNT ON -- no retorne metadatos
+	
+	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @CustomError INT
+	DECLARE @Message VARCHAR(200)
+	DECLARE @InicieTransaccion BIT
+	DECLARE @producer_id INT
+	DECLARE @count INT
+	DECLARE @point_reduction INT
+	DECLARE @address_id INT
+	
+	SET @InicieTransaccion = 0
+	IF @@TRANCOUNT=0 BEGIN
+		SET @InicieTransaccion = 1
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+		BEGIN TRANSACTION		
+	END
+	
+	BEGIN TRY
+		SET @CustomError = 2001
+
+		SELECT @address_id = address_id
+		FROM addresses WITH (TABLOCKX)
+		WHERE zip_code = @zip_code;
+		
+		WAITFOR DELAY '00:00:10';
+
+		SET @count = (SELECT COUNT(*)
+					  FROM producers AS prd WITH (TABLOCKX)
+					  JOIN addresses AS addr WITH (TABLOCKX)
+					  ON prd.address_id=addr.address_id
+					  WHERE zip_code = @zip_code);
+		SET @point_reduction = FLOOR(@sponsor_points / @count)
+		WAITFOR DELAY '00:00:10';
+
+		UPDATE prd
+		SET env_score = env_score - @point_reduction
+		FROM producers prd
+		JOIN addresses addr ON prd.address_id=addr.address_id
+		WHERE zip_code = @zip_code;
+
+		IF @InicieTransaccion=1 BEGIN
+			COMMIT
+		END
+	END TRY
+	BEGIN CATCH
+		SET @ErrorNumber = ERROR_NUMBER()
+		SET @ErrorSeverity = ERROR_SEVERITY()
+		SET @ErrorState = ERROR_STATE()
+		SET @Message = ERROR_MESSAGE()
+		
+		IF @InicieTransaccion=1 BEGIN
+			ROLLBACK
+		END
+		RAISERROR('%s - Error Number: %i', 
+			@ErrorSeverity, @ErrorState, @Message, @CustomError)
+	END CATCH	
+END
+RETURN 0
+GO
---
+++ Errors/Deadlock/update_producer_SP Original.sql
@@ -0,0 +1,68 @@
+-----------------------------------------------------------
+-- Autor: Eduardo Gutierrez
+-- Fecha: 05/01/2023
+-- Descripcion: esta description en comentarios queda almacenada
+-- Otros detalles de los parametros
+-----------------------------------------------------------
+CREATE PROCEDURE update_producer_SP
+	@og_name VARCHAR(30),
+	@new_name VARCHAR(30),
+	@new_env_score INT,
+	@new_zip_code INT
+WITH ENCRYPTION
+AS 
+BEGIN
+	
+	SET NOCOUNT ON -- no retorne metadatos
+	
+	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @CustomError INT
+	DECLARE @Message VARCHAR(200)
+	DECLARE @InicieTransaccion BIT
+	DECLARE @new_address INT
+	DECLARE @producer_id INT
+	
+	SET @InicieTransaccion = 0
+	IF @@TRANCOUNT=0 BEGIN
+		SET @InicieTransaccion = 1
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+		BEGIN TRANSACTION		
+	END
+	
+	BEGIN TRY
+		SET @CustomError = 2001
+
+		SELECT @producer_id = producer_id
+		FROM producers with (TABLOCKX)
+		WHERE name = @og_name;
+
+		WAITFOR DELAY '00:00:10';
+
+		SELECT @new_address = address_id
+		FROM addresses with (TABLOCKX)
+		WHERE zip_code = @new_zip_code;
+
+		UPDATE producers
+		SET name = @new_name,
+			env_score = @new_env_score,
+			address_id = @new_address
+		WHERE producer_id = @producer_id;
+
+		IF @InicieTransaccion=1 BEGIN
+			COMMIT
+		END
+	END TRY
+	BEGIN CATCH
+		SET @ErrorNumber = ERROR_NUMBER()
+		SET @ErrorSeverity = ERROR_SEVERITY()
+		SET @ErrorState = ERROR_STATE()
+		SET @Message = ERROR_MESSAGE()
+		
+		IF @InicieTransaccion=1 BEGIN
+			ROLLBACK
+		END
+		RAISERROR('%s - Error Number: %i', 
+			@ErrorSeverity, @ErrorState, @Message, @CustomError)
+	END CATCH	
+END
+RETURN 0
+GO
